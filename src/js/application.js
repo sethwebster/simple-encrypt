a0e3478fafde82efcee75fc6503385bb4893f261
Seth Webster <sethwebster@gmail.com>
2016-02-29 20:54:26 -0500

    Added Base 64 encoding

--- src/js/application.js
+++ src/js/application.js
@@ -4,6 +4,29 @@ console.info("| Fork me on GitHub:                        |");
 console.info("| http://tinyurl.com/simple-encrypt         |");
 console.info("+-------------------------------------------+");
 
+var encoding = {
+  none: {
+    to: function(string) {
+      return string;
+    },
+    from: function(string) {
+      return string;
+    }
+  },
+  base64: {
+    to: function(string) {
+      return window.btoa(escape(encodeURIComponent(string)));
+    },
+    from: function(string) {
+      return decodeURIComponent(unescape(window.atob(string)));
+    }
+  }
+};
+
+function updateStats() {
+  $("#stats").html($("#text").val().length);
+}
+
 function perform(action) {
   var cypher  = $("#cypher-string-input").val(),
       wrapper = $("#cypher-string-wrapper"),
@@ -22,12 +45,17 @@ function setText(text) {
 }
 
 function act(action, text, simpleEncrypt) {
+  var outputEncoder = encoding["none"];
+  if ($("#encoding-base64").is(":checked")) {
+    outputEncoder = encoding["base64"];
+  }
   if (action == "encrypt") {
-    setText(simpleEncrypt.encrypt(text))
+    setText(outputEncoder.to(simpleEncrypt.encrypt(text)));
   }
   if (action == "decrypt") {
-    setText(simpleEncrypt.decrypt(text))
+    setText(simpleEncrypt.decrypt(outputEncoder.from(text)));
   }
+  updateStats();
 }
 
 $(document).ready(function() {
@@ -36,5 +64,8 @@ $(document).ready(function() {
   });
   $("#btn-decrypt").click(function(){
     perform("decrypt");
+  });
+  $('#text').bind('input propertychange', function() {
+    updateStats();
   });
 });
